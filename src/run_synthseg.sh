69b266d759e2656f0781a432b7634ccd9b27f59c
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2022-07-08 13:18:58 +0100

    Fix typo

--- src/run_synthseg.sh
+++ src/run_synthseg.sh
@@ -26,4 +26,4 @@ while IFS=$'\t' read -r -a myArray; do
   else
     COUNTER=$[$COUNTER +1]
   fi
-done <"/project/outputs/ids/existing_samples.tsv"
+done <"/project/outputs/ids/existing_samples_preprocessed.tsv"
