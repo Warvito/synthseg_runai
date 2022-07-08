4c1be540b76ed67f47d4766a6e3e22d53df0a59f
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2022-07-08 14:43:53 +0100

    Fix requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,12 @@ ARG USER
 RUN addgroup --gid $GROUP_ID $USER
 RUN adduser --disabled-password --gecos '' --uid $USER_ID --gid $GROUP_ID $USER
 
+COPY requirements.txt .
+RUN pip3 install --upgrade pip
+RUN pip3 install -r requirements.txt
+
 RUN git clone https://github.com/BBillot/SynthSeg.git \
   && cd SynthSeg \
   && git reset --hard cd597b080eb11bdd54e4e75b28b79b41b322c0c8
-
-RUN pip3 install --upgrade pip
-RUN pip3 install -r /workspace/SynthSeg/requirements.txt
 
 WORKDIR /workspace/SynthSeg
--- cluster/runai/run_synthseg.sh
+++ cluster/runai/run_synthseg.sh
@@ -1,10 +1,10 @@
 ids_file="/project/outputs/ids/existing_samples_preprocessed.tsv"
 for i in 0; do
-  start=$((${i} * 10))
-  stop=$(((${i} + 1) * 10))
+  start=$((1+(${i} * 10)))
+  stop=$((1+((${i} + 1) * 10)))
 
   runai submit \
-    --name testing \
+    --name volume-${start}-${stop} \
     --image 10.202.67.207:5000/wds20:synthseg_runai \
     --backoff-limit 0 \
     --gpu 1 \
@@ -14,6 +14,7 @@ for i in 0; do
     --run-as-user \
     --host-ipc \
     --project wds20 \
+    --volume /nfs/home/wds20/my_temp/:/tmp/ \
     --volume /nfs/home/wds20/projects/synthseg_runai/:/project/ \
     --volume /nfs/project/danieltudosiu/results/journal_transformer_generative/ukb/ours/:/source/ \
     --volume /nfs/project/danieltudosiu/results/journal_transformer_generative/synthseg_results/ukb/ours/:/target/ \
