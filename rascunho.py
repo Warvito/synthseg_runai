18c7111ebd2229b6dd2ca5adcf580f254e3bd740
Warvito <ianonimato@hotmail.com>
2022-07-09 07:16:53 +0100

    change ids

--- rascunho.py
+++ rascunho.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 ids_df = pd.read_csv(
-    "/home/walter/Desktop/synthseg_runai/outputs/ids/existing_samples.tsv",
+    "/home/walter/Downloads/existing_samples_subset.tsv",
     sep="\t"
 )
 
@@ -9,7 +9,7 @@ ids_df["participant_id"] = ids_df["path"].str[75:86]
 ids_df["session_id"] = ids_df["path"].str[87:92]
 
 ids_df.to_csv(
-    "/home/walter/Desktop/synthseg_runai/outputs/ids/existing_samples_preprocessed.tsv",
+    "/home/walter/Downloads/existing_samples_subset_preprocessed.tsv",
     sep="\t",
     index=False
 )
