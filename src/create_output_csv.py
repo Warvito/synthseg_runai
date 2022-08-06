ced1b7353a8167e77cb59ac64c2b644bd7074e10
Warvito <ianonimato@hotmail.com>
2022-08-06 17:41:01 +0100

    Add changes

--- src/create_output_csv.py
+++ src/create_output_csv.py
@@ -3,12 +3,13 @@ import pandas as pd
 from pathlib import Path
 
 ids_df = pd.read_csv(
-    "/project/outputs/ids/existing_samples_subset_preprocessed.tsv",
+    "/project/outputs/ids/existing_samples_subset_preprocessed_lsgan.tsv",
     sep="\t"
 )
 volumes_list = []
+missing_vol = []
 for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
-    vol_path = Path(f"/target/{row['participant_id']}_{row['session_id'] }_T1w_sample/{row['participant_id']}_{row['session_id'] }_T1w_sample_decoded_vol.csv")
+    vol_path = Path(f"/target/{row['participant_id']}/{row['participant_id']}_T1w_vol.csv")
     if not vol_path.is_file():
         print(f"{str(vol_path)} does not exists!")
         continue
@@ -17,19 +18,25 @@ for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
 
     if len(volume_df) == 0:
         print(f"{str(vol_path)} does not have measures!")
+        missing_vol.append(row)
         continue
 
     volume_dict = {}
     volume_dict["participant_id"] = row['participant_id']
-    volume_dict["session_id"] = row['session_id']
     for column_name in volume_df.columns[1:]:
         volume_dict[column_name] = volume_df[column_name][0]
 
     volumes_list.append(volume_dict)
 
+missing_volumes_df = pd.DataFrame(missing_vol)
+missing_volumes_df.to_csv(
+    "/project/outputs/missing_preprocessed_lsgan.tsv",
+    sep="\t",
+    index=False
+)
 volumes_df = pd.DataFrame(volumes_list)
 volumes_df.to_csv(
-    "/project/outputs/volumes_synthseg_igpts.tsv",
+    "/project/outputs/volumes_synthseg_lsgan.tsv",
     sep="\t",
     index=False
 )
--- src/run_synthseg.sh
+++ src/run_synthseg.sh
@@ -26,4 +26,5 @@ while IFS=$'\t' read -r -a myArray; do
   else
     COUNTER=$[$COUNTER +1]
   fi
-done < "/project/outputs/ids/existing_samples_subset_preprocessed_lsgan.tsv"
+done < "/project/outputs/ids/missing_preprocessed_lsgan.tsv"
+#done < "/project/outputs/ids/existing_samples_subset_preprocessed_lsgan.tsv"
