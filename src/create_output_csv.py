ddd3bc4a695d1504d7565c299fc036dacf2b0d10
Warvito <ianonimato@hotmail.com>
2022-08-08 13:58:48 +0100

    Add changes

--- src/create_output_csv.py
+++ src/create_output_csv.py
@@ -7,6 +7,7 @@ ids_df = pd.read_csv(
     sep="\t"
 )
 volumes_list = []
+missing_vol = []
 for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
     vol_path = Path(f"/target/{row['participant_id']}_{row['session_id'] }_T1w_sample/{row['participant_id']}_{row['session_id'] }_T1w_sample_decoded_vol.csv")
     if not vol_path.is_file():
@@ -17,6 +18,7 @@ for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
 
     if len(volume_df) == 0:
         print(f"{str(vol_path)} does not have measures!")
+        missing_vol.append(row)
         continue
 
     volume_dict = {}
@@ -27,6 +29,12 @@ for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
 
     volumes_list.append(volume_dict)
 
+missing_volumes_df = pd.DataFrame(missing_vol)
+missing_volumes_df.to_csv(
+    "/project/outputs/missing_preprocessed_adni_cce.tsv",
+    sep="\t",
+    index=False
+)
 volumes_df = pd.DataFrame(volumes_list)
 volumes_df.to_csv(
     "/project/outputs/volumes_synthseg_igpts.tsv",
