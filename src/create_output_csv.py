6b7d7401a03e22930330b9cd4d7c6c0362149bd6
Warvito <ianonimato@hotmail.com>
2022-08-08 20:39:25 +0100

    Add changes

--- src/create_output_csv.py
+++ src/create_output_csv.py
@@ -7,16 +7,19 @@ ids_df = pd.read_csv(
     sep="\t"
 )
 volumes_list = []
+missing_vol = []
 for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
     vol_path = Path(f"/target/{row['participant_id']}_{row['session_id']}_T1w_sample/{row['participant_id']}_{row['session_id'] }_T1w_vol.csv")
     if not vol_path.is_file():
         print(f"{str(vol_path)} does not exists!")
+        missing_vol.append(row)
         continue
 
     volume_df = pd.read_csv(str(vol_path), sep=",")
 
     if len(volume_df) == 0:
         print(f"{str(vol_path)} does not have measures!")
+        missing_vol.append(row)
         continue
 
     volume_dict = {}
@@ -27,6 +30,12 @@ for index, row in tqdm(ids_df.iterrows(), total=ids_df.shape[0]):
 
     volumes_list.append(volume_dict)
 
+missing_volumes_df = pd.DataFrame(missing_vol)
+missing_volumes_df.to_csv(
+    "/project/outputs/missing_preprocessed_hagan.tsv",
+    sep="\t",
+    index=False
+)
 volumes_df = pd.DataFrame(volumes_list)
 volumes_df.to_csv(
     "/project/outputs/volumes_synthseg_hagan.tsv",
