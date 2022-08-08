daacb3744f6cd8bf1ae6e7c5b6ca1122693008f3
Warvito <ianonimato@hotmail.com>
2022-08-08 14:05:12 +0100

    Add changes

--- cluster/runai/run_synthseg.sh
+++ cluster/runai/run_synthseg.sh
@@ -1,32 +1,26 @@
-
-for i in {3..14}; do
-  start=$((1+(${i} * 100)))
-  stop=$((1+((${i} + 1) * 100)))
+for i in {0..34}; do
+  start=$(((${i} * 10)))
+  stop=$((((${i} + 1) * 10)))
 
   runai submit \
     --name volume-${start}-${stop} \
     --image 10.202.67.207:5000/wds20:synthseg_runai \
-    --backoff-limit 0 \
+    --backoff-limit 10 \
     --gpu 0.5 \
     --cpu 4 \
     --large-shm \
     --host-ipc \
-    --node-type "dgx2-a" \
     --project wds20 \
     --volume /nfs/home/wds20/projects/synthseg_runai/:/project/ \
-    --volume /nfs/project/danieltudosiu/results/journal_transformer_generative/ukb/our_final_igpts/:/source/ \
-    --volume /nfs/home/wds20/datasets/journal_transformer_generative/synthseg_results/our_final_igpts/:/target/ \
+    --volume /nfs/project/danieltudosiu/results/journal_transformer_generative/adni/lsgan/:/source/ \
+    --volume /nfs/home/wds20/datasets/journal_transformer_generative/synthseg_results/adni_lsgan/:/target/ \
     --command -- bash /project/src/run_synthseg.sh \
     ${start} \
     ${stop}
 done
 
-
-
-for i in {0..40}; do
-  start=$((1+(${i} * 100)))
-  stop=$((1+((${i} + 1) * 100)))
+for i in {0..34}; do
+  start=$(((${i} * 10)))
+  stop=$((((${i} + 1) * 10)))
   runai delete job volume-${start}-${stop}
 done
-
-
